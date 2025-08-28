1c319014406f2c6a257c6fa8a341557788aa9ed3
Waqar Abbas Baig <140295418+waqarbaig6133@users.noreply.github.com>
2025-08-28 05:07:58 +0000

    Update and rename code/compilers/python/parser.py to code/compilers/python/parser/dictionaries.py

---
+++ code/compilers/python/parser/dictionaries.py
@@ -0,0 +1,4 @@
+keywords = ["False", "None", "True", "and", "as", "assert", "async", "await", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try", "while", "with", "yield"]
+operators = ["-", "//", "+", "/", "*", "**", "%"]
+builtin = ['print', 'len', 'max', 'min', 'range', 'dict', 'int', 'str', 'open', 'type', 'bool', 'sum', 'any', 'all']
+
